71de705e0fe99474f692879c4c1367108f15f8be
Ifnu <ifnu.b.fatkhan@gdn-commerce.com>
2016-09-04 19:26:41 +0700

    finishing section 1.2

---
+++ section12/section12.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request){
+		fmt.Fprintf(w, "Hello, Go web development")
+	})
+
+	output := http.ListenAndServe(":8080", nil)
+
+	fmt.Println(output)
+}
